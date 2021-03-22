5fad376e2649c6c83b130c67d0d6827106804e53
Indhuja-R <81026559+Indhuja-R@users.noreply.github.com>
2021-03-22 19:00:30 +0530

    Add files via upload

---
+++ bmi-main/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_QPOIbPTY5',
+        userPoolClientId: '38n07vu9ajb5dtmgi7fm0fh6c4',
+        region: 'us-east-2'
+    },
+    api: {
+        invokeUrl: 'https://ogolhu5xqk.execute-api.us-east-2.amazonaws.com/prod'
+    }
+};
